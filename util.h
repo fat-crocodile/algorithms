df5c43f3cc9951feacfa478857579128694aac65
Sergey Kholodilov <serghol@gmail.com>
2022-03-25 05:41:17 +0300

    Add files via upload

---
+++ util.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <ctime>
+
+using namespace std;
+
+
+struct timer {
+    timer() {
+        timespec_get(&start, TIME_UTC);
+    }
+
+    long get_ms() const {
+        timespec t;
+        timespec_get(&t, TIME_UTC);
+        return (t.tv_sec - start.tv_sec) * 1000 + (t.tv_nsec - start.tv_nsec) / 1'000'000;
+    }
+
+    timespec start;
+};
+
+
+ostream& operator << (ostream& str, const vector<int>& v) {
+    str << "{";
+    if (v.size() > 0) {
+        size_t n = (v.size() <= 52)? v.size() : 50;
+        for (int i = 0; i < n - 1; ++i) {
+            str << v[i] << ", ";
+        }
+
+        if (n < v.size()) {
+            cout << "..., ";
+        }
+
+        str << v.back();
+    }
+    str << "}";
+    return str;
+}
+
+
+
+
+
+
+
